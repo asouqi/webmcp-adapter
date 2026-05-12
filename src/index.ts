ebaf4b1ac230b901cf0ef9e6305aa5b6a48c6854
asouqi <asouqi@atypon.com>
2026-05-12 03:05:37 +0300

    export standard schema check

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { ToolDefinition, ToolConfig, UnregisterFn, ValidationResult, StandardSchema } from "./types"
 import { isWebMCPSupported } from "./utils"
 import { defineTool, registerTool, registerBatch, unregisterTool, unregisterAllTools} from "./tool"
-import { validateJsonSchema } from "./validator"
+import { validateJsonSchema, isStandardSchema, validateWithStandardSchema } from "./validator"
 
 export type { InputSchema, JsonValue } from '@mcp-b/webmcp-types'
 
